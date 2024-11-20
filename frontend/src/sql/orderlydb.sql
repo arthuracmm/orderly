e4753455d8b7035dc813b2000259c859ddade629
hugozera1 <hugorodrigues1052@gmail.com>
2024-11-20 10:28:32 -0300

    readme update

--- frontend/src/sql/orderlydb.sql
+++ frontend/src/sql/orderlydb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 20/11/2024 às 13:46
+-- Tempo de geração: 20/11/2024 às 14:27
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.0.30
 
@@ -68,7 +68,9 @@ INSERT INTO `login` (`id`, `nome`, `email`, `password`, `cpf`, `cep`, `telefone`
 (3, 'Carlos Souza', 'carlos.souza@email.com', 'senha789', '45678912345', '23456789', '11932165487', 'Rua C, 789, Bairro Y', 0),
 (8, 'adm', 'admin@ex.com.br', 'admin', '0', '0', '0', 'nenhum', 1),
 (9, 'uriel', 'urielaraujo@gmai.com', '1234', '50932049', '14403446', '1698999456', 'Minha casa ', 0),
-(10, 'Gabriel', 'gabrielmessias27@gmail.com', '123', '52026147823', '14402435', '16994282051', 'Benedito barbosa 1160', 0);
+(10, 'Gabriel', 'gabrielmessias27@gmail.com', '123', '52026147823', '14402435', '16994282051', 'Benedito barbosa 1160', 0),
+(11, 'teste', 'teste@teste.com', '123', '1111', '111', '11111', 'teste', 0),
+(12, 'guilhermy', 'guilhemy1309@gmail.com', '123', '52739866', '103245', '1699499492', 'Sao paulo', 0);
 
 --
 -- Índices para tabelas despejadas
@@ -89,7 +91,7 @@ ALTER TABLE `login`
 -- AUTO_INCREMENT de tabela `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
